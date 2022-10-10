44603bbf767523e318ba615106d49b7c5148f1be
rcrcue <115020933+rcrcue@users.noreply.github.com>
2022-10-10 20:27:37 +0300

    Update admins.py

--- program/admins.py
+++ program/admins.py
@@ -154,7 +154,7 @@ async def mute(client, m: Message):
 
 
 @Client.on_message(
-    command(["رفع الكتم", f"unmute@{BOT_USERNAME}", "unmute"]) & other_filters
+    command(["رفع الكتم", f"unmute@{BOT_USERNAME}", "/unmute"]) & other_filters
 )
 @authorized_users_only
 async def unmute(client, m: Message):
